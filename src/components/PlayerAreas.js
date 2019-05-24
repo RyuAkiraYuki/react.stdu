9c0290cac8ea2be72affd53ccfd01d733476e54e
RyuAkiraYuki <rurounikenshinyuki@gmail.com>
2019-05-25 00:56:55 +0300

    Another lost commit

--- src/components/PlayerAreas.js
+++ src/components/PlayerAreas.js
@@ -4,12 +4,11 @@ import StyleProperties from './StyleProperties';
 
 class PlayerAreas extends Component {
   render() {
-      console.log(this.props.pickerz)
     return this.props.areas.map((area)=>(
         <div key={area.id} className={'tool-item ' + area.type}>
           <div className="area-title">{area.title}</div>
           <AreaElements  structure={area.elements}/>
-          <StyleProperties styles={area.styleProps}/>
+          <StyleProperties structure={area.styleProps}/>
         </div>
     ));
   }
